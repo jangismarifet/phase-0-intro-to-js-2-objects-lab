e5307ef3c62cf81f6a7ee60ebf7cdba8283dca3b
jangismarifet <jangismarifet@gmail.com>
2022-04-24 22:56:26 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let employee = {
+    name: "Jangis",
+    streetAddress: "555 Broadway"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmp = {...employee};
+    newEmp[key] = value;
+    return newEmp;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmp = {...employee};
+    delete newEmp[key];
+    return newEmp;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
